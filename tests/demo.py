aba9a05a618bbb86a3ce7827657617d97fec4099
RockJohnson503 <836867547@qq.com>
2019-06-13 14:35:29 +0800

    修改样例

--- tests/demo.py
+++ tests/demo.py
@@ -4,7 +4,12 @@
 File: test.py
 Author: Rock Johnson
 """
-from panicbuying.panicbuying.panic import Panic
+try:
+    # windows
+    from panicbuying.panicbuying.panic import Panic
+except:
+    # linux
+    from panicbuying.panic import Panic
 
 if __name__ == '__main__':
     xm = Panic(store='小米', driver='chromedriver', account='你的账户', password='你的密码', goods_name='你需要抢购的商品', nth=1)
