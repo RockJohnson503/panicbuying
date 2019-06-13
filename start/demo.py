a1c1d17d5a8e82019293fa1a4aef9b2dd7415af7
RockJohnson <rockjohnson270@gmail.com>
2019-06-13 16:41:07 +0800

    添加说明

--- start/demo.py
+++ start/demo.py
@@ -13,6 +13,8 @@ except:
     from panicbuying.panic import Panic
 
 if __name__ == '__main__':
+    # chromedriver下载地址: http://npm.taobao.org/mirrors/chromedriver/
+
     xm = Panic(store='小米', driver='chromedriver', account='你的账户', password='你的密码', goods_name='你需要抢购的商品', nth=1)
     xm.start()
     # xm.close()
