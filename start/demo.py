5ef2eee14864c767cc1a41ebe62e866c8012a08e
RockJohnson503 <836867547@qq.com>
2019-06-14 13:11:45 +0800

    修改案例

--- start/demo.py
+++ start/demo.py
@@ -15,6 +15,9 @@ except:
 if __name__ == '__main__':
     # chromedriver下载地址: http://npm.taobao.org/mirrors/chromedriver/
 
-    xm = Panic(store='小米', driver='chromedriver', account='你的账户', password='你的密码', goods_name='你需要抢购的商品', nth=1)
+    xm = Panic(
+        store='小米', driver='chromedriver', account='你的账户', password='你的密码',
+        goods_name='你需要抢购的商品', goods_nth=1, addr_nth=1
+    )
     xm.start()
     # xm.close()
